eadfecfa5180c4206e5600e94570853324c1fc73
mattesalva93 <salvalaggiomatteo93@gmail.com>
2022-01-11 15:59:32 +0100

    like button color working, no counter

--- main.js
+++ main.js
@@ -56,7 +56,6 @@ const posts = [
     }
 ];
 
-
 function generaPost(mioArray){
     const creaPost = document.querySelector(".posts-list");
     creaPost.innerHTML += `
@@ -79,7 +78,7 @@ function generaPost(mioArray){
             <div class="post__footer">
                 <div class="likes js-likes">
                     <div class="likes__cta">
-                        <a class="like-button  js-like-button" href="#" data-postid="1">
+                        <a class="like-button js-like-button" data-postid="1">
                             <i class="like-button__icon fas fa-thumbs-up" aria-hidden="true"></i>
                             <span class="like-button__label">Mi Piace</span>
                         </a>
@@ -93,7 +92,27 @@ function generaPost(mioArray){
     `
 }
 
+function aggiungiLike(){
+    const tastoLike = document.getElementsByClassName("js-like-button");
+    console.log(tastoLike);
+
+    for (let i = 0; i < tastoLike.length; i++){
+        tastoLike[i].addEventListener("click", function(){
+            this.classList.add("like-button--liked");
+             
+
+        })
+
+    }
+}
+
+
+
 for (i = 0; i < posts.length; i++){
     generaPost(posts);
 }
+
+aggiungiLike();
+ 
+
 
